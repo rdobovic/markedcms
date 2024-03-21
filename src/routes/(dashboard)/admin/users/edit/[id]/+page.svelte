8cf14263e019be05a5938b2c7ded3cd82bf0a22b
rdobovic <roko@brownbird.eu>
2024-03-21 08:48:21 +0100

    Fixed user edit form

--- src/routes/(dashboard)/admin/users/edit/[id]/+page.svelte
+++ src/routes/(dashboard)/admin/users/edit/[id]/+page.svelte
@@ -1,48 +1,42 @@
 <script>
+    import { formStore } from '$lib/stores/formStore.js';
+
     import Form from '$lib/components/Form.svelte';
     import FormInput from '$lib/components/FormInput.svelte';
     import FormSection from '$lib/components/FormSection.svelte';
     import Button from '$lib/components/Button.svelte';
 
-    import getOldFunction from '$lib/helpers/getOldFunction.js';
-
     export let data;
     export let form;
 
-    let pass, passRetype;
-    $: old = getOldFunction(data.user, form?.data);
+    const formData = formStore({ name: 'users-edit-form' });
 
-    const handleSubmit = () => {
-        pass = passRetype = '';
-    }
+    $: formData.setErrors(form?.errors ?? {});
+    $: formData.setValues({
+        ...(data?.user ?? {}),
+        ...(form?.data ?? {}),
+    });
 
 </script>
 
-<Form on:submit={handleSubmit}>
+<Form store={formData}>
     <FormSection title="Edit user">
         <FormInput
             type="text"
             disabled={true}
             name="username"
-            value={data.user.username}
         >Username</FormInput>
         <FormInput
             type="text"
             name="displayName"
-            value={old('displayName')}
-            errors={form?.errors?.displayName}
         >Display name</FormInput>
         <FormInput
             type="password"
             name="password"
-            bind:value={pass}
-            errors={form?.errors?.password}
         >Password</FormInput>
         <FormInput
             type="password"
             name="passwordRetype"
-            bind:value={passRetype}
-            errors={form?.errors?.passwordRetype}
         >Retype password</FormInput>
 
         <div slot="buttons" class="flex w-full gap-2">
